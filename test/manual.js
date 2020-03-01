da54b04ffe81d2d99dbd1181c25fceec895aed63
Heru Joko Utomo <heru@wallextech.com>
2020-03-01 13:48:21 +0800

    add quick sort and build workflow

--- test/manual.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var quick_sort_1 = require("../src/strategies/quick.sort");
-var bubble = new quick_sort_1.QuickSort();
-// const testData = [1,2, 5, 4, -1, -2, 3];
-var testData = [7, 3, 8, 4, 9];
-// const correctResult = [3, 4, 7, 8, 9];
-console.log("result = " + bubble.sort(testData));
