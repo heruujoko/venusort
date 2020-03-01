8c77c6b5e5408d12742b96b29b0e2f628934ab95
Heru Joko Utomo <heru@wallextech.com>
2020-03-01 13:42:02 +0800

    add random test for bubble sort

--- test/bubble.sort.test.ts
+++ test/bubble.sort.test.ts
@@ -1,5 +1,9 @@
 import { BubbleSort } from '../src/strategies/bubble.sort';
 
+function getRandomInt(max: number) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
 describe('Bubble Sort', () => {
   it('should sort simple number arrays', () => {
     const testData = [7,3,8,4,9];
@@ -15,5 +19,16 @@ describe('Bubble Sort', () => {
 
     const bubble = new BubbleSort();
     expect(bubble.reverseSort(testData)).toEqual(correctResult);
+  });
+
+  it('should pass test with 20 random number given', () => {
+    const testData = [];
+    for (let i=0;i < 20;i++) {
+      testData.push(getRandomInt(20));
+    }
+
+    const correctResult = testData.sort(function(a, b){return a-b});
+    const bubble = new BubbleSort();
+    expect(bubble.sort(testData)).toEqual(correctResult);
   });
 });
